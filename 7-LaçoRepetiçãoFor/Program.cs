cb70f70738fb02546454eb24aec0ad3fa04da6f5
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-12-20 15:56:10 -0300

    Laço de Repetição for.

---
+++ 7-LaçoRepetiçãoFor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_LaçoRepetiçãoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7");
+
+            double valorInvestido = 1000;
+            
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+            }
+
+            //For encadeado
+            double valorInvestido2 = 1000;
+            double fatorRendimento = 1.0036;
+
+            for(int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido2 *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido2);
+
+            Console.ReadLine();
+        }
+    }
+}
