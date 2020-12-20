caece955e970ff9297d070905aab4acbf15ac59e
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-12-20 15:16:31 -0300

    Condicional if.

---
+++ 5-CondicionalIF/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5");
+
+            int idade = 17;
+            Boolean acompanhanteAduto = true;
+
+            if(idade >= 18)
+            {
+                Console.WriteLine("Pode entrar!");
+            } else if(idade < 18 && acompanhanteAduto)
+            {
+                Console.WriteLine("Pode entrar acompanhado!");
+            } else
+            {
+                Console.WriteLine("Não pode entrar")
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
