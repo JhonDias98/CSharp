4d3ed1a18d61eff513638748c52de3d5d7c42873
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-12-20 15:30:24 -0300

    Laço de repetição while.

---
+++ 6-LaçoRepetiçãoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_LaçoRepetiçãoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while(contadorMes <= 12)
+            {
+                valorInvestido += valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá " + valorInvestido);
+
+                contadorMes += 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
